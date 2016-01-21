04c9ca78677fbde6fe9169c2f311be3aaab84f8a
rxdtxd <rxdtxd@wemakethings.net>
2016-01-21 19:59:56 +0200

    src: motor_step() inline function, some code rearrangements, iterator var in fakedelay() is now volatile.

--- src/main.c
+++ src/main.c
@@ -13,7 +13,7 @@
 // FIXME: move elsewhere
 // FIXME: cycle overhead is huge for _delay_us()
 void fakedelay (uint16_t times) {
-    uint16_t i;
+    volatile uint16_t i;
     for (i = 0; i < times; i++) {
 	_delay_us(1); // FIXME: magicnum
     }
@@ -21,9 +21,31 @@ void fakedelay (uint16_t times) {
 }
 
 
+inline void motor_step (uint8_t motor, uint16_t speed) {
+    uint8_t i;
+    uint8_t motorpin;
+
+    // FIXME: due to pinout motorport will have to be set, too
+    if (motor > 4) return;
+    if (motor == 0) motorpin = MOTOR0_STEP;
+    /* if (motor == 1) motorpin = MOTOR1_STEP; */
+    /* if (motor == 2) motorpin = MOTOR2_STEP; */
+    /* if (motor == 3) motorpin = MOTOR3_STEP; */
+    /* if (motor == 4) motorpin = MOTOR4_STEP; */
+	
+    for (i = 0; i < 255; i++) { // FIXME: 255 = maxval uint8_t
+	output_high(MOTORS_PORT,motorpin);
+	fakedelay(speed);
+	output_low(MOTORS_PORT,motorpin);
+	fakedelay(speed);
+    }
+    return;
+}
+
+
 int main (void) {
     uint8_t i;
-    uint16_t motorspeed;
+    uint16_t speed;
     uint8_t buttonsup, buttonsdown;
     
     leds_init();
@@ -41,11 +63,22 @@ int main (void) {
     set_input(MOTORS_DDR, MOTOR0_DOWN_DD);
     
     while (1) {
-	motorspeed = adc_read(6);
+	speed = adc_read(6);  // FIXME: iterate through 5 ADC pins
 
+	// debug (1 chan)
 	buttonsup = MOTOR0_PIN & _BV(MOTOR0_UP);
 	buttonsdown = MOTOR0_PIN & _BV(MOTOR0_DOWN);
 
+	/* // TODO: func() read into shift registers from buttons */
+	/* shiftreg_mode_load(); */
+	/* _delay_us(0.05); // 165 datasheet says 15 ns minimum */
+	/* shiftreg_mode_transmit(); */
+	/* _delay_us(0.05); */
+	
+	/* // read from shift registers into microcontroller */
+	/* buttonsup = spi_transmit(SPI_TRANSMIT_DUMMY); */
+	/* buttonsdown = spi_transmit(SPI_TRANSMIT_DUMMY); */
+	
 	// set dir
 	// FIXME: buttons{up,down} check functions
 	// FIXME set_dir() functions
@@ -60,23 +93,8 @@ int main (void) {
 	
 	// roll motor
 	if ((buttonsup > 0) != (buttonsdown > 0)) {
-	    for (i = 0; i < 255; i++) { // FIXME: 255 = max uint8_t
-		output_high(MOTORS_PORT,MOTOR0_STEP);
-		fakedelay(motorspeed);
-		output_low(MOTORS_PORT,MOTOR0_STEP);
-		fakedelay(motorspeed);
-	    }
+	    motor_step(0, speed);
 	}
-        
-	// TODO: func() read into shift registers from buttons
-	shiftreg_mode_load();
-	_delay_us(0.05); // 165 datasheet says 15 ns minimum
-	shiftreg_mode_transmit();
-	_delay_us(0.05);
-	
-	/* // read from shift registers into microcontroller */
-	/* buttons_up = spi_transmit(SPI_TRANSMIT_DUMMY); */
-	/* buttons_down = spi_transmit(SPI_TRANSMIT_DUMMY); */
     }
     
     return 0;
