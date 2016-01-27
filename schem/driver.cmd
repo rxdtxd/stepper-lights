1fbd4cca11d30ef1e8ddb585da00f534b7a33c84
rxdtxd <rxdtxd@wemakethings.net>
2016-01-27 12:49:13 +0200

    schem/driver: generated .cmd, .net, .pcb files.

---
+++ schem/driver.cmd
@@ -0,0 +1,96 @@
+# Pin name action command file
+
+# Start of element CONN_STEPPER
+ChangePinName(CONN_STEPPER, 1, 1)
+ChangePinName(CONN_STEPPER, 4, 4)
+ChangePinName(CONN_STEPPER, 2, 2)
+ChangePinName(CONN_STEPPER, 3, 3)
+
+# Start of element C1
+ChangePinName(C1, 2, -)
+ChangePinName(C1, 1, +)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)
+
+# Start of element C3
+ChangePinName(C3, 2, -)
+ChangePinName(C3, 1, +)
+
+# Start of element C4
+ChangePinName(C4, 2, 2)
+ChangePinName(C4, 1, 1)
+
+# Start of element C2
+ChangePinName(C2, 2, -)
+ChangePinName(C2, 1, +)
+
+# Start of element R12
+ChangePinName(R12, 1, 1)
+ChangePinName(R12, 2, 2)
+
+# Start of element C12
+ChangePinName(C12, 2, 2)
+ChangePinName(C12, 1, 1)
+
+# Start of element U12
+ChangePinName(U12, 5, GND)
+ChangePinName(U12, 8, VCC)
+ChangePinName(U12, 1, R)
+ChangePinName(U12, 7, B/DO*/RI*)
+ChangePinName(U12, 6, A/DO/RI)
+ChangePinName(U12, 4, D)
+ChangePinName(U12, 3, DE)
+ChangePinName(U12, 2, RE*)
+
+# Start of element R11
+ChangePinName(R11, 1, 1)
+ChangePinName(R11, 2, 2)
+
+# Start of element C11
+ChangePinName(C11, 2, 2)
+ChangePinName(C11, 1, 1)
+
+# Start of element U2
+ChangePinName(U2, 3, 3)
+ChangePinName(U2, 2, 2)
+ChangePinName(U2, 1, 1)
+
+# Start of element U11
+ChangePinName(U11, 5, GND)
+ChangePinName(U11, 8, VCC)
+ChangePinName(U11, 1, R)
+ChangePinName(U11, 7, B/DO*/RI*)
+ChangePinName(U11, 6, A/DO/RI)
+ChangePinName(U11, 4, D)
+ChangePinName(U11, 3, DE)
+ChangePinName(U11, 2, RE*)
+
+# Start of element CONN0
+ChangePinName(CONN0, 8, I7)
+ChangePinName(CONN0, 7, I6)
+ChangePinName(CONN0, 6, I5)
+ChangePinName(CONN0, 5, I4)
+ChangePinName(CONN0, 4, I3)
+ChangePinName(CONN0, 3, I2)
+ChangePinName(CONN0, 2, I1)
+ChangePinName(CONN0, 1, I0)
+
+# Start of element U1
+ChangePinName(U1, 8, VMOT)
+ChangePinName(U1, 16, DIR)
+ChangePinName(U1, 7, GND)
+ChangePinName(U1, 6, B2)
+ChangePinName(U1, 1, GND)
+ChangePinName(U1, 9, \_EN\_)
+ChangePinName(U1, 15, STEP)
+ChangePinName(U1, 14, \_SLP\_)
+ChangePinName(U1, 12, M2)
+ChangePinName(U1, 11, M1)
+ChangePinName(U1, 10, M0)
+ChangePinName(U1, 5, B1)
+ChangePinName(U1, 4, A1)
+ChangePinName(U1, 3, A2)
+ChangePinName(U1, 13, \_RST\_)
+ChangePinName(U1, 2, \_FAULT\_)
