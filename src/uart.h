ddad9766421453799bbeaccaf6e08a464d442895
rxdtxd <rxdtxd@wemakethings.net>
2016-02-01 19:45:41 +0200

    src: actually add uart.{c,h}.

---
+++ src/uart.h
@@ -0,0 +1,14 @@
+// http://efundies.com/avr-and-printf/
+// http://www.appelsiini.net/2011/simple-usart-with-avr-libc
+
+#include <stdio.h>
+
+void uart_putchar(char c, FILE *stream);
+char uart_getchar(FILE *stream);
+
+void uart_init(void);
+
+/* http://www.ermicro.com/blog/?p=325 */
+
+FILE uart_output = FDEV_SETUP_STREAM(uart_putchar, NULL, _FDEV_SETUP_WRITE);
+//FILE uart_input = FDEV_SETUP_STREAM(NULL, uart_getchar, _FDEV_SETUP_READ);
